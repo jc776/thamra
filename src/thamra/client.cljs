9ed925c9cde323e0d58beb6b9c68f007d3ad82da
Jack Coady <jack.coady@cantab.net>
2019-11-22 22:26:20 +0000

    use citrus for events + effects

--- src/thamra/client.cljs
+++ src/thamra/client.cljs
@@ -1,7 +1,9 @@
 (ns ^:dev/always thamra.client
-  (:require [cljs.repl :as repl]
+  (:require [thamra.app :as app]
             #_[thamra.editor :as editor]
             [thamra.dom :as d]
+			[cljs.repl :as repl]
+			[citrus.core :as citrus]
             ["react" :as react]
             ["react-dom" :as react-dom]))
 
@@ -29,12 +31,27 @@
   (str (with-out-str (repl/source fact))
        "\n"
 	   "(fact 5)"))
+	   
+;; create Reconciler instance
+(defonce reconciler
+  (citrus/reconciler
+    {:state
+     (atom {}) ;; application state
+     :controllers
+     {:counter app/control} ;; controllers
+     :effect-handlers
+     {:local-storage app/local-storage}})) ;; effect handlers
+
+;; initialize controllers
+(defonce init-ctrl (citrus/broadcast-sync! reconciler :init))
 
 (def el 
   (d/<> {}
     (Hello {:key "hello" :name "world!"})
 	
 	(Counter {:key "count" :text "Count: "})
+	
+	(app/AppCounter {:r reconciler})
 	
 	(d/div {:key "text" :style {"fontWeight" "bold"}} "Hello " "there!")
 	
